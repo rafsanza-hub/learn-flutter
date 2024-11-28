93da120d29814cd8e73c2d4dddaa603fe5d9b04b
Rafsan <rapsanza@gmail.com>
2024-11-29 02:40:33 +0700

    Learn Layout Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// void main() => runApp(const MyApp());
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       home: HomePage(),
-//     );
-//   }
-// }
-
-// class HomePage extends StatefulWidget {
-//   const HomePage({super.key});
-
-//   @override
-//   HomePageState createState() => HomePageState();
-// }
-
-// class HomePageState extends State<HomePage> {
-//   int _selectedIndex = 0;
-
-//   // Method untuk mengubah halaman berdasarkan index
-//   void _onItemTapped(int index) {
-//     setState(() {
-//       _selectedIndex = index;
-//     });
-//   }
-
-//   // Konten untuk setiap halaman
-//   final List<Widget> _pages = [
-//     const Center(
-//       child: Text(
-//         'Home Page',
-//         style: TextStyle(fontSize: 24),
-//       ),
-//     ),
-//     const Center(
-//       child: Text(
-//         'Search Page',
-//         style: TextStyle(fontSize: 24),
-//       ),
-//     ),
-//     const Center(
-//       child: Text(
-//         'Profile Page',
-//         style: TextStyle(fontSize: 24),
-//       ),
-//     ),
-//   ];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("Flutter Navigation Example"),
-//       ),
-//       body: _pages[_selectedIndex], // Menampilkan halaman sesuai index
-//       bottomNavigationBar: BottomNavigationBar(
-//         currentIndex: _selectedIndex,
-//         onTap: _onItemTapped,
-//         items: const <BottomNavigationBarItem>[
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.home),
-//             label: 'Home',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.search),
-//             label: 'Search',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.account_circle),
-//             label: 'Profile',
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -90,72 +7,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-  void _clicked(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  final List<Widget> _pages = [
-    const Center(
-      child: Text(
-        'Home Page',
-        style: TextStyle(fontSize: 24),
-      ),
-    ),
-    const Center(
-      child: Text(
-        'Search Page',
-        style: TextStyle(fontSize: 24),
-      ),
-    ),
-    const Center(
-      child: Text(
-        'Profile Page',
-        style: TextStyle(fontSize: 24),
-      ),
-    ),
-  ];
-
-  @override
-  Widget build(BuildContext contect) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Learn Navigatian"),
-      ),
-      body: _pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _clicked,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Profile',
-          ),
-        ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo'),
+        ),
+        body: Stack(
+          children: [
+            Container(
+              color: Colors.amber,
+              height: 200,
+              width: 200,
+            ),
+            Container(
+              color: Colors.blue,
+              height: 100,
+              width: 100,
+            ),
+            Container(color: Colors.red, height: 50, width: 50),
+          ],
+        ),
       ),
     );
   }
