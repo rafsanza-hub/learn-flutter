2bc070e85147fe9a89a5247e1d5693d17bd0b667
Rafsan <rapsanza@gmail.com>
2024-11-30 02:16:48 +0700

    mapping-list, card, decoration

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _count = 0;
-  void _incrementCounter() {
-    setState(() {
-      _count++;
-    });
-  }
-
-  void _decrementCounter() {
-    if (_count > 0) {
-      setState(() {
-        _count--;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Homme Page"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(_count.toString()),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: _decrementCounter,
-                  child: const Text("-"),
-                ),
-                ElevatedButton(
-                  onPressed: _incrementCounter,
-                  child: const Text("+"),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
