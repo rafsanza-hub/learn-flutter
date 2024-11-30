6a7cef57756c996bf16151906322491e52b74560
Rafsan <rapsanza@gmail.com>
2024-11-30 16:48:55 +0700

    DismissibleWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,85 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
-import 'package:second_app/home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Faker faker = Faker();
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: HomePage(faker: faker),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+    required this.faker,
+  });
+
+  final Faker faker;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dismissible"),
+      ),
+      body: ListView.separated(
+        separatorBuilder: (context, index) => const Divider(),
+        itemCount: 100,
+        itemBuilder: (context, index) {
+          return Dismissible(
+            confirmDismiss: (direction) {
+              return showDialog(
+                  context: context,
+                  builder: (index) {
+                    return AlertDialog(
+                      title: const Text("Confirm"),
+                      content: const Text("Are you sure you want to delete?"),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context, false);
+                          },
+                          child: const Text("No"),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context, true);
+                          },
+                          child: const Text("Yes"),
+                        )
+                      ],
+                    );
+                  });
+            },
+            direction: DismissDirection.endToStart,
+            background: Container(
+              color: Colors.red,
+              padding: const EdgeInsets.only(right: 20),
+              alignment: Alignment.centerRight,
+              child: const Icon(
+                Icons.delete,
+                size: 25,
+              ),
+            ),
+            key: Key(index.toString()),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://picsum.photos/id/${index + 100}/200/200"),
+              ),
+              title: Text(faker.person.name()),
+              subtitle: Text(faker.lorem.sentence()),
+            ),
+          );
+        },
+      ),
     );
   }
 }
