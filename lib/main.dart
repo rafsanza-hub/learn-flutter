98e504ea2b547352e66ae97855df4bf25b6855aa
Rafsan <rapsanza@gmail.com>
2024-11-30 14:05:18 +0700

    TextField

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String hasil = "";
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("MyApp"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextField(
+                onSubmitted: (value) {
+                  setState(() {
+                    hasil = value;
+                  });
+                },
+              ),
+              Text(hasil),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
