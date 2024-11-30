6e3ac1e13689131362e36be2d45225bb9076447a
Rafsan <rapsanza@gmail.com>
2024-11-30 14:45:22 +0700

    Grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,66 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  List<Container> grid = List.generate(100, (index) {
+    return Container(
+      height: 100,
+      width: 100,
+      color: Color.fromARGB(
+        Random().nextInt(255),
+        Random().nextInt(255),
+        Random().nextInt(255),
+        Random().nextInt(255),
+      ),
+    );
+  });
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String hasil = "";
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("MyApp"),
-          backgroundColor: Colors.blue,
+          title: Text("Grid"),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              TextField(
-                onSubmitted: (value) {
-                  setState(() {
-                    hasil = value;
-                  });
-                },
-              ),
-              Text(hasil),
-            ],
-          ),
-        ),
+        // body: GridView(
+        //   padding: const EdgeInsets.all(20),
+        //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        //       crossAxisCount: 3,
+        //       crossAxisSpacing: 20,
+        //       mainAxisSpacing: 20,
+        //       childAspectRatio: 1 / 1),
+        //   children: grid,
+        // ),
+        // body: GridView.count(
+        //   padding: const EdgeInsets.all(20),
+        //   crossAxisCount: 3,
+        //   crossAxisSpacing: 20,
+        //   mainAxisSpacing: 20,
+        //   children: grid,
+        // ),
+        body: GridView.builder(
+            padding: const EdgeInsets.all(20),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+            ),
+            itemBuilder: (context, int index) {
+              return Container(
+                  height: 100,
+                  width: 100,
+                  color: Color.fromARGB(
+                    Random().nextInt(255),
+                    Random().nextInt(255),
+                    Random().nextInt(255),
+                    Random().nextInt(255),
+                  ));
+            }),
       ),
     );
   }
